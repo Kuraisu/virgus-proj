b96c486433db2c59630c882d0bab059ff90c76f2
Aleksei Ermakov <jp.kuraisu@gmail.com>
2017-10-20 12:48:41 +0300

    Adds babel and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,29 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
